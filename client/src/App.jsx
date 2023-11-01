f4ddf6d589d8eac5838acc5d3520333ad3fd2df1
azubuike <chimafrancis94@gmail.com>
2023-11-01 13:31:18 +0100

    Create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,19 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import SignUp from "./Pages/SignUp";
-import SignOut from "./Pages/SignOut";
 import Profile from "./Pages/Profile";
+import Header from "./Component/Header";
+import SignIn from "./Pages/SignIn";
 
 const App = () => {
   return (
     <BrowserRouter >
+      <Header/>
       <Routes>
         <Route path="/" element= {<Home/>} />
         <Route path="/about" element= {<About/>} />
         <Route path="/sign-up" element= {<SignUp/>} />
-        <Route path="/sign-out" element= {<SignOut/>} />
+        <Route path="/sign-in" element= {<SignIn/>} />
         <Route path="profile" element= {<Profile/>} />
       </Routes>
     </BrowserRouter>
---
+++ client/src/Component/Header.jsx
@@ -0,0 +1,36 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="text-bold text-sm sm:text-xl flex flex-wrap">
+          <span className="text-slate-500">Chima</span>
+          <span className="text-slate-700">Estate</span>
+        </h1>
+        <form className="bg-slate-100 p-3 rounded-full flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-500" />
+        </form>
+        <ul className="flex gap-4">
+          <li className="hidden sm:inline hover:underline text-slate-700">
+            <Link to={"/"}>Home </Link>
+          </li>
+          <li className=" hover:underline text-slate-700">
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li className=" hover:underline text-slate-700">
+            <Link to={"/sign-in"}>Sign in</Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
