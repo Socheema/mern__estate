a06268a028a452dcab31e729064be4c885287355
azubuike <chimafrancis94@gmail.com>
2023-11-01 14:36:18 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
-import express from "express"
-const app = express()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+// bRdEUuHUMCWAKmFy
+mongoose
+  .connect(process.env.MONGO__URI)
+  .then(() => {
+    console.log("Connect to MondoDB!!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000!!!")
-})
+  console.log("Server is running on port 3000!!!");
+});
