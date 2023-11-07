194c3033bbe41891d26f6ffe5d933d8aeb34c129
azubuike <chimafrancis94@gmail.com>
2023-11-07 16:29:53 +0100

    update user api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/User.route.js";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
+import cookieparser from "cookie-parser"
 
 mongoose
   .connect(process.env.MONGO__URI)
@@ -17,6 +18,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieparser())
 
 const PORT = process.env.PORT || 3000;
 
---
+++ api/utils/verifyToken.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import { errorHandler } from "./error.js";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  console.log(token)
+  if (!token) return next(errorHandler(401, "You are not authorized"));
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, "Forbidden"));
+    req.user = user;
+    console.log(user.id)
+    next()
+  });
+};
